17ab5eca7814b3825c7810ff07b806fcdf1d760c
knterada5_mac <knterada5@gmail.com>
2022-12-21 16:58:39 +0900

    sleep

--- ok/test.py
+++ ok/test.py
@@ -3,7 +3,9 @@ import time
 print('line true')
 
 print('\r1 gyoume', end='')
-time.sleep(1)
+time.sleep(2)
 print('\r2 gyoume')
+time.sleep(2)
 print('3 gyoume')
+time.sleep(2)
 print('\r4 gyoume')
